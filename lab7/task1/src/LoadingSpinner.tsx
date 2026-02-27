b8b2da2e5045157935b954c7867d1202f1c0aa3f
assemmin <107746223+assemmin@users.noreply.github.com>
2026-02-27 14:00:03 +0500

    docs: labs7

---
+++ lab7/task1/src/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+function LoadingSpinner() {
+    return (
+      <div style={{ textAlign: "center", padding: "2rem" }}>
+        <div className="spinner"></div>
+        <p>Loading page...</p>
+      </div>
+    );
+  }
